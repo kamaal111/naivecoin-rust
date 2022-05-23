6f9889137e4c86ecb55c9954ebc2b40a81b80bbe
Kamaal <kamaal.f1@gmail.com>
2022-05-23 21:53:57 +0200

    Initializing blockchain with a genesis block

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
 mod models;
-use models::block::Block;
+use models::blockchain::Blockchain;
 
 fn main() {
-    let genesis_block = Block {
-        index: 0,
-        hash: String::from("cd2fb2ace926608315b2a5bd1bc2a259dce057a21ed63351adc0b1326da2a99e"),
-        previous_hash: None,
-        timestamp: 1652722519,
-        data: String::from("The Genesis block!!!"),
-    };
-
-    println!("{:#?}", genesis_block);
+    let blockchain = Blockchain::new();
+    println!("{:#?}", blockchain);
 }
